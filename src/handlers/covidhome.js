6c44c13bfed15801f2ea1d56389af0470af9ab8f
Kuttiraja Veeranan <kuttiraja@gmail.com>
2020-04-25 17:22:13 -0500

    changes

--- src/handlers/covidhome.js
+++ src/handlers/covidhome.js
@@ -15,10 +15,20 @@ async function covidAssistant(req, res, next) {
     parameters = req.body.queryResult ? (req.body.queryResult.parameters ?req.body.queryResult.parameters : null)  : null
 
     if(action!= null && action != ""){
-        let personResponse = new employeeModel(parameters)
+        let personResponse = {
+          travelOutside: parameters.travelOutside === "false" ? false : true,
+          usedPublicTransport: parameters.usedPublicTransport === "false" ? false : true,
+          havingCovid: parameters.havingCovid === "false" ? false : true,
+          havingFever: parameters.havingFever === "false" ? false : true,
+          employeeId: parameters.employeeId,
+          employeeName: parameters.employeeName
+        }
+        
+        let assistantDetails = new employeeModel(personResponse)
 
         try {
-            await personResponse.save();
+
+            await assistantDetails.save();
             logger.info("User Details saved")
         }
         catch(err) {
